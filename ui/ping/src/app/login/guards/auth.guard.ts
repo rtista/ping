3efcbc5fb4c9d1ec32987db0105bc8237c099c02
goncaloreais <goncalorsd@gmail.com>
2020-04-04 02:12:28 +0100

    feat: adds auth guard

---
+++ ui/ping/src/app/login/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+    ) {}
+
+    canActivate() {
+        this.router.navigate(['/login']);
+        return false;
+    }
+}
